495918cb7f31f97d673cf1cadffed60cc3287aab
warrioryoko <miangpellegri@gmail.com>
2020-10-01 03:51:17 -0700

    Finished table and comments model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,8 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('alchemy-app routes', () => {
+describe('tardigram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 });
---
+++ sql/setup.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS users, posts, comments;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS INDENTITY PRIMARY KEY,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL,
+  profile_photo_url TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT NOT NULL REFERENCES users(id),
+  photo_url TEXT NOT NULL,
+  caption TEXT NOT NULL,
+  tags TEXT[]
+);
+
+CREATE TABLE comments (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  comment_by BIGINT NOT NULL REFERENCES users(id),
+  post_id BIGINT NOT NULL REFERENCES post(id),
+  comment TEXT NOT NULL
+);
